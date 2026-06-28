1779f0c5818818ff289140b4e565c45ab1d51d6c
ukinabr <snowsnowsnowsnow902@gmail.com>
2026-06-28 17:57:58 -0300

    Update loader.lua

--- dist/loader.lua
+++ dist/loader.lua
@@ -1,6 +1,5 @@
-if game.PlaceId ~= 93557410403539 then
+if game.PlaceId ~= 93557410403539 and game.GameId ~= 7516718402 then
     return
 end
 
-local url = "https://raw.githubusercontent.com/SorUmPlayer/Project-Sor/refs/heads/main/Noob-Must-Die/lobby.lua"
-loadstring(game:HttpGet(url))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SorUmPlayer/Project-Sor/refs/heads/main/Noob-Must-Die/lobby.lua"))()
